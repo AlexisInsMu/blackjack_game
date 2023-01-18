710a802626dbc63f09689b85e5fbe38489718596
ALEXIS ADRIAN MURILLO BARRIENTOS <amurillob2000@alumno.ipn.mx>
2023-01-18 13:10:21 -0600

    dictionary of the cards

--- blackjack_game/Program.cs
+++ blackjack_game/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Dictionary<string, int> MyCards = new Dictionary<string, int>() 
+{ 
+    {"as", 1}, {"1", 1}, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, {"8", 8},{"9", 9},{"10", 10},{"joto", 11},
+}; //dictionary with the names and values of the cards
+
