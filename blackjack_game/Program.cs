5410a206a5c0f04007752e10fc44a58f0f561fac
ALEXIS ADRIAN MURILLO BARRIENTOS <amurillob2000@alumno.ipn.mx>
2023-01-18 14:39:27 -0600

    function distribute, base game

--- blackjack_game/Program.cs
+++ blackjack_game/Program.cs
@@ -1,8 +1,54 @@
 ﻿/*// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");*/
-
-Dictionary<string, int> MyCards = new Dictionary<string, int>() 
-{ 
+Random random = new Random();
+Dictionary<string, int> TheCards = new Dictionary<string, int>()
+{
     {"as", 1}, {"1", 1}, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, {"8", 8},{"9", 9},{"10", 10},{"jack", 11},{"queen", 12},{"king", 13},
 }; //dictionary with the names and values of the cards
+Dictionary<string, int> distribute()
+{
+    Dictionary<string, int> interiorsCards = new Dictionary<string, int>();
+    int num1 = random.Next(0, 13);
+    int num2 = random.Next(0, 13);
+    bool numControl1 = false;
+    bool numControl2 = false;
+    foreach (KeyValuePair<string, int> kvp in TheCards)
+    {
+        if (num1 == kvp.Value && !numControl1)
+        {
+            interiorsCards.Add(kvp.Key, kvp.Value);
+            numControl1 = true;
+        }
+        else if (num2 == kvp.Value && !numControl2)
+        {
+            interiorsCards.Add(kvp.Key, kvp.Value);
+            numControl2 = true;
+        }
+        else if (numControl1 && numControl2)
+        {
+            break;
+        }
+    }
+    return interiorsCards;
+}
+
+
+
+Dictionary<string, int> myCards = new Dictionary<string, int>();
+Dictionary<string, int> cupierCards = new Dictionary<string, int>();
+while (true)
+{
+    myCards = distribute();
+    cupierCards = distribute();
+    Console.WriteLine(myCards.Count);
+    Console.WriteLine(cupierCards.Count);
+    Console.WriteLine("Desea continuar?\n presione true si es asi o false si no");
+    bool stop = Convert.ToBoolean(Console.ReadLine());
+    if (!stop)
+    {
+        break;
+    }
+}
+
+
 
