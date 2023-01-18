1db9fb07075159190d1a38c39e9c1fdf40ab7356
ALEXIS ADRIAN MURILLO BARRIENTOS <amurillob2000@alumno.ipn.mx>
2023-01-18 13:13:56 -0600

    fix in the dictionary

--- blackjack_game/Program.cs
+++ blackjack_game/Program.cs
@@ -1,8 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");*/
 
 Dictionary<string, int> MyCards = new Dictionary<string, int>() 
 { 
-    {"as", 1}, {"1", 1}, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, {"8", 8},{"9", 9},{"10", 10},{"joto", 11},
+    {"as", 1}, {"1", 1}, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, {"8", 8},{"9", 9},{"10", 10},{"jack", 11},{"queen", 12},{"king", 13},
 }; //dictionary with the names and values of the cards
 
