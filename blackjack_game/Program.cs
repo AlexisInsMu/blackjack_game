cbd7bc87582280ae41ef4383e40bcecb4d17cd17
ALEXIS ADRIAN MURILLO BARRIENTOS <amurillob2000@alumno.ipn.mx>
2023-01-28 15:35:39 -0600

    function lectura

--- blackjack_game/Program.cs
+++ blackjack_game/Program.cs
@@ -4,12 +4,9 @@ Console.WriteLine("Hello, World!");*/
 Random random = new Random();
 Dictionary<string, int> TheCards = new Dictionary<string, int>()
 {
-    {"as", 1}, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, {"8", 8},{"9", 9},{"10", 10},{"jack", 11},{"queen", 12},{"king", 13}
+    {"as", 1}, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, {"8", 8},{"9", 9},{"10", 10},{"jack", 10},{"queen", 10},{"king", 10}
 }; //dictionary with the names and values of the cards
-Dictionary<string, int> myCards = new Dictionary<string, int>()
-{
-    {"as", 1}, { "2", 2 }, { "3", 3 }, { "4", 4 }, { "5", 5 }, { "6", 6 }, { "7", 7 }, {"8", 8},{"9", 9},{"10", 10},{"jack", 11},{"queen", 12},{"king", 13}
-};
+Dictionary<string, int> myCards = new Dictionary<string, int>();
 Dictionary<string, int> cupierCards = new Dictionary<string, int>();
 (string, int) distribute()
 {
@@ -54,7 +51,7 @@ void compareCards(bool control)
         }
         else
         {
-            cupierCards.Add(valores.Item1 + Convert.ToString(cupierCards.Count), valores.Item2);
+            cupierCards.Add(valores.Item1 + " " + Convert.ToString(cupierCards.Count), valores.Item2);
         }
     }
 } // this functionn check if the dictionary have repeat values
@@ -63,30 +60,48 @@ int myValue = 0;
 int cupierValue = 0;
 while (true)
 {
+    Console.WriteLine("Desea jugar una partida?\nEscribe true to say yes or false to say no");
     bool Control = Convert.ToBoolean(Console.ReadLine());
-    //if()
-    compareCards(true);
-    compareCards(true);
-    compareCards(false);
-    compareCards(false);
-    foreach (KeyValuePair<string, int> kvp in myCards)
+    if (Control)
     {
-        Console.WriteLine($"Key user= {kvp.Key}");
-        myValue += kvp.Value;
+        compareCards(true);
+        compareCards(true);
+        compareCards(false);
+        compareCards(false);
+        while (true)
+        {
+            foreach (KeyValuePair<string, int> kvp in myCards)
+            {
+                Console.WriteLine($"Key user= {kvp.Key}");
+                myValue += kvp.Value;
+            }
+            Console.WriteLine("Estas son tus cartas");
+            foreach (KeyValuePair<string, int> kvp in cupierCards)
+            {
+                Console.WriteLine($"Key cupier= {kvp.Key}");
+                cupierValue += kvp.Value;
+            }
+            if (myValue == 21)
+            {
+                Console.WriteLine("You win");
+            }
+            if(cupierValue == 21)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+
+            }
+        }
     }
-    foreach (KeyValuePair<string, int> kvp in cupierCards)
+    else
     {
-        Console.WriteLine($"Key cupier= {kvp.Key}");
-        cupierValue += kvp.Value;
+
     }
     Console.WriteLine("my value: {0}, cupier value: {1}", myValue, cupierValue);
     Console.WriteLine("Desea continuar?\n presione true si es asi o false si no");
-    bool stop = Convert.ToBoolean(Console.ReadLine());
-    if (!stop)
-    {
-        Console.WriteLine("adios");
-        break;
-    }
+
     else
     {
         Console.WriteLine("ya vete");
